8ab14b9b1ead9377182a3ec916ce02b3252c94ce
accountsware <muhammad@accountsware.com>
2015-04-05 22:15:06 +0100

    Changed .IdentityServerLearning.Identity to Web Application from Class Library

--- IdentityServerLearning.Identity/Properties/AssemblyInfo.cs
+++ IdentityServerLearning.Identity/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7377890-2e05-4841-88fe-9645993396b4")]
+[assembly: Guid("45fe8ba7-6641-4d23-8ac4-9e731546cce7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
